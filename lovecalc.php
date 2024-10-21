4878de42d53cf77cade521afe33ab5cd8cf00231
elvakalela <elva.23129@mhs.unesa.ac.id>
2024-10-21 09:33:26 +0700

    Add files via upload

---
+++ lovecalc.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="lovecalcstyle.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
+    <title>Couple A More</title>
+</head>
+<body>
+    <nav>
+        <div class="logo-navigasi">
+            <img src="logolovecalc.png">
+        </div>
+        <input type="checkbox" id="menu-checkbox">
+        <label for="menu-checkbox" class="menu-icon">
+            <i class='bx bx-menu'></i>
+        </label>
+        <ul class="navbar">
+            <li><a href="lovecalc.php">Home</a></li>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="byname.php">Love Calc by Name</a></li>
+            <li><a href="byzodiac.php">Love Calc by Zodiac</a></li>
+        </ul>
+    </nav>
+
+    <div class="home">
+        <img src="couple.png">
+        <h1>Discover Your Love Compatibility Instantly!</h1>
+        <h3>Find out how well you and your partner match.<br>Just enter your names and let the magic of love calculation reveal your compatibility score!</h3>
+        <a href="byname.php" class="btn-calc">Calculate Now</a>
+    </div>
+
+    <div class="explanation">
+        <img src="question.png">
+        <h2>What is a Love Calculator?</h2>
+        <p>A Love Calculator is a fun and entertaining tool that uses a simple algorithm to estimate the compatibility between two people based on their names. Whether you’re curious about a new crush or want to see how well you match with your long-term partner, this calculator offers a playful way to explore your romantic potential. While it’s all in good fun, who knows? You might discover a spark of truth in your results!</p>
+    </div>
+
+    <div class="how-to">
+        <img src="how-to.png">
+        <h2>How to Play?</h2>
+        <p>You just need to enter your name and your partner's name, whether it's your crush or your boyfriend. Then, click the "Calculate" button to determine the level of compatibility between you and your partner.</p>
+    </div>
+
+    <footer>
+        <div class="logo-footer">
+            <img src="logolovecalc.png">
+            <p>&copy; 2024 Couple A More</p>
+        </div>
+        <ul class="footer-nav">
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="faq.html">FAQ</a></li>
+            <li><a href="pp.html">Privacy Policy</a></li>
+            <li><a href="supportus.php">Support Us</a></li>
+        </ul>
+        <div class="icon-footer">
+            <a href="https://www.instagram.com/lindatriwdnt">
+                <i class='bx bxl-instagram-alt'></i>
+            </a>
+            <a href="https://www.tiktok.com/@elvakalela">
+                <i class='bx bxl-tiktok' ></i>
+            </a>
+            <a href="https://wa.me/6281235804218">
+                <i class='bx bxl-whatsapp'></i>
+            </a>
+        </div>
+    </footer>
+
+</body>
+</html>
